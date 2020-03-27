9c33e7e261ac32d756978337f54585ef6a04ce91
adaacin <61974004+adaacin@users.noreply.github.com>
2020-03-27 11:10:31 +0100

    Create JavaScriptTask1.js

---
+++ JavaScriptTask1.js
@@ -0,0 +1,14 @@
+let name = "Ada David-Worlu";
+const courses = ["go","php","csharp","html", "css", "js", "design","flutter", "java" , "kotlin"];
+
+ console.log("My name is: " + name,"\n");
+ console.log("My courses are: " + course,"\n");
+ 
+ const EvenNos = [];
+
+  for (i=1; i<=200; i++) {
+    if (i % 2 === 0) {
+        EvenNos.push(i);
+    }
+  }
+  console.log("My total nos of courses is an even number so let me now display all even numbers from 1 - 200 = "+ EvenNos);
